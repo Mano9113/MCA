e85b27cab0d5f362119714373414547696a5850d
Mano9113 <manoranjanke39@gmail.com>
2024-12-27 19:25:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 db(); 
 
-app.post('/checkUser2', async (req, res) => {
+app.get('/checkUser2', async (req, res) => {
   console.log("working...")
 });
 
