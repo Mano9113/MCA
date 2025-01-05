19107c6765da8aba9a05d4d9bff31ff47de6c639
Mano9113 <manoranjanke39@gmail.com>
2025-01-05 20:04:08 +0530

    Update StudentMarks.js

--- backend/models/StudentMarks.js
+++ backend/models/StudentMarks.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose'); // Import mongoose
 const StudentMarksSchema = new mongoose.Schema({
   Name: { type: String, required: true }, // Allow duplicates
   Class: { type: String, sparse: true }, // Optional field
-  Date: { type: String, required: true }, // Allow duplicates
+  Date: { type: Date, required: true }, // Allow duplicates
   KANNADA: { type: Number, required: true },
   ENGLISH: { type: Number, required: true },
   PHYSICS: { type: Number, required: true },
